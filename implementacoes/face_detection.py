d103c7ff7d8bcf9338c7b62c636c17c3f2c56b85
IsmaelCesar <leamscesar@gmail.com>
2019-06-12 22:28:51 -0300

    Hair detection with too many outliers

--- implementacoes/face_detection.py
+++ implementacoes/face_detection.py
@@ -17,7 +17,7 @@ image_selected= ['person1.jpg',
 
 global width, height
 
-def hair_detection(image,H,I):
+def hair_detection(image,H,I,ArcCoss):
     mImage = image.copy()
     #Normalization
     #mImage = mImage/255
@@ -46,11 +46,42 @@ def hair_detection(image,H,I):
 
     return  mImage
 
+def hair_detection2(image,H,I,ArcCoss,NormRGB):
+    global rows, cols
+    kImage = image.copy()
+    mImage = kImage.copy() / 255
+    mImage = np.array([[NormRGB(mImage[i][j][2], mImage[i][j][1], mImage[i][j][0]) for j in range(cols)] for i in range(rows)])
+    rImage = mImage[:, :, 2]
+    gImage = mImage[:, :, 1]
+    bImage = mImage[:, :, 0]
+
+
+    angles = ArcCoss(rImage,gImage,bImage)
+    applyH = np.array([[H(bImage[i][j],gImage[i][j],angles[i][j]) for j in range(cols)] for i in range(rows)])
+    #applyI = I(rImage,gImage,bImage)
+    applyI = I(kImage[:,:,2], kImage[:,:,1], kImage[:,:,0])
+    # Computing B - G and B - R
+    #bmg = bImage - gImage
+    #bmr = bImage - rImage
+    bmr = kImage[:, :, 0] - kImage[:, :, 2]
+    bmg = kImage[:,:,0] - kImage[:,:,1]
+
+    for i in range(rows):
+        for j in range(cols):
+            condition = (applyI[i,j] < 80)
+            condition = condition and (bmg[i,j] < 15 or bmr[i,j] < 15)
+            condition = condition or (applyH[i,j] > np.pi/3 and applyH[i,j] <= 1/3*np.pi )#2/9*np.pi
+            if not condition:
+                kImage[i, j, 0] = 0.
+                kImage[i, j, 1] = 0.
+                kImage[i, j, 2] = 0.
+
+    return kImage
 
 def skin_detection(image,F1,F2,White,H,ArcCos,NormRGB):
     global rows, cols
     kImage = image.copy()
-    mImage = kImage.copy()/255
+    mImage = kImage.copy()
     mImage = np.array([[NormRGB(mImage[i][j][2], mImage[i][j][1], mImage[i][j][0]) for j in range(cols)] for i in range(rows)])
     rImage = mImage[:, :, 2]
     gImage = mImage[:, :, 1]
@@ -106,7 +137,7 @@ def getI():
 
 def main():
     global rows, cols
-    image = cv2.imread(images_folder + image_selected[0])
+    image = cv2.imread(images_folder + image_selected[3])
     rows = image.shape[0]
     cols = image.shape[1]
 
@@ -118,10 +149,12 @@ def main():
     I = getI()
     White = whiteRange()
 
-    newDetect = skin_detection(image,F1,F2,White,H,ArcCos,NormRGB)
+    #newDetect = skin_detection(image,F1,F2,White,H,ArcCos,NormRGB)
+    newDetect = hair_detection2(image,H, I, ArcCos, NormRGB)
 
     #cv2.imshow("Original",image)
-    cv2.imshow("Detection(Skin)",newDetect)
+    #cv2.imshow("Detection(Skin)",newDetect)
+    cv2.imshow("Detection(Hair)", newDetect)
     cv2.waitKey(0)
     # resultI = I(rImage,gImage,bImage)
 
